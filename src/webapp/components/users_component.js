90bed1a3986d18002bf9d745a2a5bc8b8c536eb3
Juan haugaard <juanhaugaard@netzero.net>
2018-04-13 00:20:07 -0400

    more of the users selector

--- src/webapp/components/users_component.js
+++ src/webapp/components/users_component.js
@@ -1,52 +1,53 @@
 import React, { Component } from 'react';
 
 class User extends Component {
-    render() {
-        <option value={this.props.id}>{this.props.value}</option>
-    }
+  render() {
+    return <option value={this.props.value}>{this.props.value}</option>;
+  }
 }
 
-
 function userList(users) {
-    if (!users) {
-        return 'users is undefined';
-    } else { 
-        return (
-            users.map(
-                user => {
-                    return ( {
-                        key=user.identifier,
-                        <User 
-                            id={user.identifier} 
-                            value={user.identifier} 
-                        />
-                    }
-                    );
-                }
-            )
-        )
-    }
+  if (!users) {
+    return 'users is undefined';
+  } else {
+    return (
+      users.map(
+        user => {
+          return (
+            <User
+              key={user.identifier}
+              value={user.identifier}
+            />
+          );
+        }
+      )
+    )
+  }
 }
 
 class Users extends Component {
-    constructor(props) {
-        super(props);
-        console.log('Users constructor called, props=' + this.props);
-    }
 
-    render() {
-        var propsDefined = 'true';
-        if (!this.props)
-            propsDefined = 'false';
-        return (
-            <div className='search-bar'>
-                Users component rendered {propsDefined}
-                {<select>
-                    {userList(this.props.users)}
-                </select>}
-            </div>
-        );
-    }
+  handleChangeFnc(event) {
+    console.log('handleChange called: ' + event.target.value);
+    var newState = Object.assign({}, this.props.state);
+    newState.selectedUser = event.target.value;
+    this.setState(newState);
+  }
+
+  render() {
+    this.handleChange = this.handleChangeFnc.bind(this);
+    return (
+      <span>
+        <select
+          style={{marginLeft: 10 + 'px', marginRight: 10 + 'px'}}
+          className = 'figure'
+          onChange={this.handleChange} >
+          {userList(this.props.users)}
+        </select>
+        Selected user: {this.props.selectedUser}
+      </span>
+    );
+  }
 }
 
 export default Users;
--- src/webapp/index.js
+++ src/webapp/index.js
@@ -9,43 +9,46 @@ require.context('./style/', true, /^\.\/.*\.css$/);
 require.context('./images/', true, /^\.\/.*\.(jpg|png|svg)$/);
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            selectedUser: null,
-            privileges: [],
-            selectedPrivileges: [],
-            scopes: [],
-            selectedScope: null,
-            assignemnts: [],
-            selectedAssignment: null
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      selectedUser: '**',
+      privileges: [],
+      selectedPrivileges: [],
+      scopes: [],
+      selectedScope: null,
+      assignemnts: [],
+      selectedAssignment: null
+    };
+  }
 
-    componentDidMount() {
-        // fetchSubjects.bind(this)();
-        fetchSubjects((subjectsJson) => {
-            console.log('subjects: ' + subjectsJson);
-            var newState = Object.assign({}, this.state);
-            newState.users = subjectsJson;
-            this.setState(newState);
-        });
-    }
+  componentDidMount() {
+    // fetchSubjects.bind(this)();
+    fetchSubjects((subjectsJson) => {
+      console.log('subjects: ' + subjectsJson);
+      var newState = Object.assign({}, this.state);
+      newState.users = subjectsJson;
+      this.setState(newState);
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <Title />
-                <span>
-                    <div>Users:</div>
-                    <Users
-                        users = {this.state.users}
-                    />
-                </span>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Title />
+        <div>
+          <span>Users:</span>
+          <Users
+            users={this.state.users}
+            selectedUser={this.state.selectedUser}
+            setState={this.setState}
+            state={this.state}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
